07ff6c26046def3a37cbf6d3020378594366fc82
Dennizz <root@ipaddr.nl>
2022-02-04 11:19:50 +0100

    changing output of IP's to an instance in stead of a list

--- copilot_build_aws/outputs.tf
+++ copilot_build_aws/outputs.tf
@@ -1,9 +1,9 @@
 output private_ip {
-  value       = aws_instance.aviatrixcopilot.*.private_ip
+  value       = aws_instance.aviatrixcopilot.private_ip
   description = "Private IP address of the Aviatrix Copilot"
 }
 
 output public_ip {
-  value       = aws_eip.copilot_eip.*.public_ip
+  value       = aws_eip.copilot_eip.public_ip
   description = "Public IP address of the Aviatrix Copilot"
 }
