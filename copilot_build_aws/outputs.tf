ba624c83d5cb035cb0a981cbdcf7049ea39cc6fc
Dennis Hagens <dennis@aviatrix.com>
2022-02-28 20:41:44 +0100

    changing output of IP's to an instance in stead of a list (#10)
    Co-authored-by: Shuo Liu <sliu@aviatrix.com>

--- copilot_build_aws/outputs.tf
+++ copilot_build_aws/outputs.tf
@@ -4,11 +4,11 @@ output "ec2-info" {
 }
 
 output "private_ip" {
-  value       = aws_instance.aviatrixcopilot.*.private_ip
+  value       = aws_instance.aviatrixcopilot.private_ip
   description = "Private IP address of the Aviatrix Copilot"
 }
 
 output "public_ip" {
-  value       = aws_eip.copilot_eip.*.public_ip
+  value       = aws_eip.copilot_eip.public_ip
   description = "Public IP address of the Aviatrix Copilot"
 }
