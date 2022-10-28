7d9d111b1181ac8b21540a7ac767da5422ca2301
Shuo Liu <sliu@aviatrix.com>
2022-10-27 22:20:18 -0700

    AVX-31251: fix bug in TF OCI copilot module output (#27)

--- copilot_build_oci/outputs.tf
+++ copilot_build_oci/outputs.tf
@@ -1,3 +1,7 @@
+output "availability_domain" {
+  value = data.oci_identity_availability_domain.ad.name
+}
+
 output "public_ip" {
   value = oci_core_instance.copilot_vm.public_ip
 }
@@ -14,22 +18,27 @@ output "nsg_id" {
   value = oci_core_network_security_group.nsg.id
 }
 
-output "security_list_id" {
-  value = oci_core_vcn.copilot_vcn[0].default_security_list_id
-}
-
-output "availability_domain" {
-  value = data.oci_identity_availability_domain.ad.name
-}
-
 output "vcn_id" {
-  value = oci_core_vcn.copilot_vcn.id
+  value = var.use_existing_vcn ? var.vcn_id : oci_core_vcn.copilot_vcn[0].id
 }
 
-output "route_table_id" {
-  value = oci_core_route_table.copilot_rt[0].id
+data "oci_core_vcn" "vcn" {
+  vcn_id = var.use_existing_vcn ? var.vcn_id : oci_core_vcn.copilot_vcn[0].id
+}
+
+output "security_list_id" {
+  value = data.oci_core_vcn.vcn.default_security_list_id
 }
 
 output "dhcp_options_id" {
-  value = oci_core_vcn.copilot_vcn[0].default_dhcp_options_id
+  value = data.oci_core_vcn.vcn.default_dhcp_options_id
+}
+
+data "oci_core_route_tables" "route_table" {
+  compartment_id = var.compartment_ocid
+  display_name = var.routetable_display_name
+}
+
+output "route_table_id" {
+  value = data.oci_core_route_tables.route_table.id
 }
