70688b3ba73caeaff8a346467c184e91dfe15348
Rodman <rodmans@aviatrix.com>
2023-05-01 13:43:11 -0700

    Update variables.tf

--- copilot_build_aws/variables.tf
+++ copilot_build_aws/variables.tf
@@ -155,7 +155,9 @@ data "http" "copilot_iam_id" {
 data "aws_availability_zones" "all" {}
 
 data "aws_ec2_instance_type_offering" "offering" {
-  for_each = toset(data.aws_availability_zones.all.names)
+  for_each = {
+    us-east-1a = "us-east-1a" 
+  }
   
   filter {
     name   = "instance-type"
