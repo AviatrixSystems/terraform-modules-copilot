7994d7b9bd5b0f0a1be8289a73bb95c551ddd774
Jim McDonald <jmcdonald@spike-lnx.home.fearthepenguin.net>
2023-10-11 11:45:40 -0500

    Use POSIX compatible commands in script waiting for copilot

--- copilot_build_aws/main.tf
+++ copilot_build_aws/main.tf
@@ -178,8 +178,8 @@ count=0
 until [ "$(curl -ks https://${try(aws_eip.copilot_eip[0].public_ip, "")}/api/info/updateStatus | jq -r '.status')" = "finished" ]
 do
   sleep 10
-  ((count++))
-  if [[ $count -eq 60 ]]; then
+  count=$((count+1))
+  if [ $count -eq 60 ]; then
     break
   fi
 done
